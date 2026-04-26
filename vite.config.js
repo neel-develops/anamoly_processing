c4555f8388670a1dd6ee7817a041ca69e39101b4
Neel Sonawane <sothisishowhedied@gmail.com>
2026-04-26 18:04:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/anamoly_processing/',  // must match your repo name exactly
   plugins: [react()],
-  base: '/anamoly_processing/',
 })
